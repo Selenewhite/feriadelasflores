12640de9a1fc9169f7c79912310180db8abc2cbc
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2013-07-14 23:22:19 -0500

    Se obtienen los eventos vía AJAX
    Se obtienen los eventos y se filtran en la petición ajax para mostrar
    solo los que cumplen con el criterio de la fecha seleccionada. Falta
    agregar detalles visuales como el ajax loader y el current en el
    calendario.

--- wp-content/themes/feria/category-4.php
+++ wp-content/themes/feria/category-4.php
@@ -83,14 +83,13 @@
   </div><!-- cierra .EventosDestacados --> 
   
   <!-- CIERRA CARRUSEL EVENTOS DESTACADOS -->   
-  
+  </div> <!-- CIERRE OCULTAR PROGRAMACIÓN -->
   
   
 
   
   <div class="clearfix row-fluid">  
   <div class="span6 calendario"></div>	
-  <div class="span6 calendario"></div>
   </div><!-- cierra .row-fluid --> 
   
   <div>
@@ -166,54 +165,7 @@
           </ul>
   </div>
   
-  </div> <!-- CIERRE OCULTAR PROGRAMACIÓN -->
-  
-  </div> <!-- #container -->
-  
-  <script src="<?php bloginfo('template_directory'); ?>/jquery.isotope.min.js"></script>
-  <script>
-  $(function(){
-  
-  var $container = $('#container');
-  
-  $container.isotope({
-  itemSelector : '.element'
-  });
-  
-  
-  var $optionSets = $('#options .option-set'),
-  $optionLinks = $optionSets.find('a');
-  
-  $optionLinks.click(function(){
-  var $this = $(this);
-  // don't proceed if already selected
-  if ( $this.hasClass('selected') ) {
-  return false;
-  }
-  var $optionSet = $this.parents('.option-set');
-  $optionSet.find('.selected').removeClass('selected');
-  $this.addClass('selected');
-  
-  // make option object dynamically, i.e. { filter: '.my-filter-class' }
-  var options = {},
-  key = $optionSet.attr('data-option-key'),
-  value = $this.attr('data-option-value');
-  // parse 'false' as false boolean
-  value = value === 'false' ? false : value;
-  options[ key ] = value;
-  if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-  // changes in layout modes need extra logic
-  changeLayoutMode( $this, options )
-  } else {
-  // otherwise, apply new options
-  $container.isotope( options );
-  }
-  
-  return false;
-  });
-  });
-  </script>
-  
+  </div> <!-- #container -->  
   
   </div>
 
@@ -223,5 +175,5 @@
   
   </div> <!-- end #content -->
   </div> <!-- end #container -->
-  
+
   <?php get_footer(); ?>
---
+++ wp-content/themes/feria/getEvento.php
@@ -0,0 +1,53 @@
+<?php
+define('WP_USE_THEMES', false);
+require_once('../../../wp-load.php');
+
+// The Query
+/*
+$myQuery = new WP_Query(array(
+     'meta_key' => 'fecha_inicio', 
+     'meta_value' => $_POST['fecha']
+));*/
+$myQuery = new WP_Query(array(
+     'cat' => 10
+));
+// The Loop
+while ( $myQuery->have_posts() ) : $myQuery->the_post();
+$fechaSeleccionada = str_replace('/', '-', $_POST['fecha']);
+$fechaSeleccionada = strtotime($fechaSeleccionada);
+$fechaInicio = str_replace('/', '-', get_post_meta($post->ID,'fecha_inicio',true));
+$fechaInicio = strtotime($fechaInicio);
+$fechaFin = str_replace('/', '-', get_post_meta($post->ID,'fecha_fin',true));
+$fechaFin = strtotime($fechaFin);
+
+$horaInicio = get_post_meta($post->ID,'hora_inicio',true);
+$horaFin = get_post_meta($post->ID,'hora_fin',true);
+$jornada = get_post_meta($post->ID,'jornada',true);
+
+$jornadas = explode(',', $jornada);
+$humanHoraInicio = DATE("g:i a", STRTOTIME($horaInicio));
+$humanHoraFin = DATE("g:i a", STRTOTIME($horaFin));
+
+$class = "";
+foreach ($jornadas as $j) {
+	$class .= trim($j) . " ";
+}
+
+?>
+  <?php if($fechaSeleccionada >= $fechaInicio AND $fechaSeleccionada <= $fechaFin): ?>
+  <div class="element <?php echo $class ?>">
+  
+          <h2><?php the_title(); ?></h2>  
+          <ul>
+	          <li><i class="icon-time"></i> <strong> Hora:</strong> <?php echo $humanHoraInicio ?> a <?php echo $humanHoraFin ?></li>
+	          <li><i class="icon-map-marker"></i> <strong>Lugar:</strong> Unidad deportiva de Belén, Unidad Deportiva María Luisa Calle, Coliseo Carlos Mauro Hoyos, Pista de Motrocross.</li>
+	          <li><i class="icon-star"></i> <strong>Teléfono:</strong> 369 90 00 Ext 182 </li>
+          </ul>
+  </div>
+  <?php endif; ?>
+<?php 
+endwhile;
+
+// Reset Query
+wp_reset_query();
+?>
