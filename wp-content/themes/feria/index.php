23665a8ecd540e8f17e94607ba88d852a3910f4e
Selenewhite <seleneestrada@gmailcom>
2013-08-06 18:03:09 -0500

    Diseño de galeria para "Hoy en la feria"

--- wp-content/themes/feria/index.php
+++ wp-content/themes/feria/index.php
@@ -24,54 +24,56 @@ if ($blog_hero){
       <div class="slider visible-desktop">
         <?php echo get_new_royalslider(1); ?>
       </div><!-- cierra .slider -->
+      
+      
+   
 
-      <div class="hoy">
+        <div class="hoy">
         <!-- Titulos -->
         <div class="clearfix row-fluid">
           <div class="titHoy span8">
             <span class="icono hidden-phone"></span>
             <span>Hoy en la feria</span>         
           </div><!-- cierra .titHoy -->  
-
-      
-
-          <div class="span4 titManana hidden-phone">
-            <span>No te pierdas mañana...</span>  
-          </div><!-- cierra .titManana --> 
-        </div>
-        <!-- Galerías -->    
-        <div class="clearfix row-fluid">
-          <div class=" span8 galeriaHoy">
           
-          	 <!-- CARRUSEL EVENTOS DESTACADOS -->
-    <!-- Carousel
-    ================================================== -->
+          <div class="span4 titManana hidden-phone">
+              <span>No te pierdas mañana...</span>  
+            </div><!-- cierra .titManana --> 
+
+         </div>
+         
+           
+        <div class="clearfix row-fluid">
+  
+          <div class="span8 galeriaHoy">
+          
+           	 <!-- CARRUSEL EVENTOS DESTACADOS -->
     <div id="myCarousel" class="carousel slide span12">
       <!-- Indicators -->
-      <ol class="carousel-indicators">
+     <!-- <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
+      </ol>-->
       <div class="carousel-inner">
         <div class="item active">
           <ul class="thumbnails">
             <li class="span4">
               <div class="thumbnail">
-                <img src="http://lorempixel.com/250/250" alt="">
-                <div>XiX encuentro internacional Matancero</div>
+                <img src="http://lorempixel.com/250/250" alt="" class="img-rounded">
+                <div class="textoGaleria">XiX encuentro internacional Matancero</div>
               </div>
             </li>
             <li class="span4">
               <div class="thumbnail">
-                <img src="http://lorempixel.com/250/250" alt="">
-                <div>Temporada Baile Paisa</div>
+                <img src="http://lorempixel.com/250/250" alt="" class="img-rounded">
+                <div class="textoGaleria">Temporada Baile Paisa</div>
               </div>
             </li>
             <li class="span4">
               <div class="thumbnail">
-                <img src="http://lorempixel.com/250/250" alt="">
-                <div>Orquídeas, Pájaros y Flores Feria de Artesanías, XX exposición Feria de las Flores (coctel de inauguración)</div>
+                <img src="http://lorempixel.com/250/250" alt="" class="img-rounded">
+                <div class="textoGaleria">Orquídeas, Pájaros y Flores Feria de Artesanías, XX exposición Feria de las Flores (coctel de inauguración)</div>
               </div>                            
             </li>            
           </ul>
@@ -80,20 +82,20 @@ if ($blog_hero){
           <ul class="thumbnails">
             <li class="span4">
               <div class="thumbnail">
-                <img src="http://lorempixel.com/250/250" alt="">
-                <div>Tertulias en Torno a la Arriería y Patrimonio de San Cristó- bal</div>
+                <img src="http://lorempixel.com/250/250" alt="" class="img-rounded">
+                <div class="textoGaleria">Tertulias en Torno a la Arriería y Patrimonio de San Cristó- bal</div>
               </div>
             </li>
             <li class="span4">
               <div class="thumbnail">
-                <img src="http://lorempixel.com/250/250" alt="">
-                <div>Semana Cultural Silletera</div>
+                <img src="http://lorempixel.com/250/250" alt="" class="img-rounded">
+                <div class="textoGaleria">Semana Cultural Silletera</div>
               </div>
             </li>
             <li class="span4">
               <div class="thumbnail">
-                <img src="http://lorempixel.com/250/250" alt="">
-                <div>Primer Festival de la Trova Silletero de oro</div>
+                <img src="http://lorempixel.com/250/250" alt="" class="img-rounded">
+                <div class="textoGaleria">Primer Festival de la Trova Silletero de oro</div>
               </div>                            
             </li>            
           </ul>
@@ -103,19 +105,19 @@ if ($blog_hero){
             <li class="span4">
               <div class="thumbnail">
                 <img src="http://lorempixel.com/250/250" alt="">
-                <div>Festival Xiii Mamimuti</div>
+                <div class="textoGaleria">Festival Xiii Mamimuti</div>
               </div>
             </li>
             <li class="span4">
               <div class="thumbnail">
                 <img src="http://lorempixel.com/250/250" alt="">
-                <div>XXv Concurso de Fotografría de Fauna</div>
+                <div class="textoGaleria">XXv Concurso de Fotografría de Fauna</div>
               </div>
             </li>
             <li class="span4">
               <div class="thumbnail">
                 <img src="http://lorempixel.com/250/250" alt="">
-                <div>Muestra de Flores, Folclor, Artesanías y Sabores de Piedras Blancas</div>
+                <div class="textoGaleria">Muestra de Flores, Folclor, Artesanías y Sabores de Piedras Blancas</div>
               </div>                            
             </li>            
           </ul>
@@ -125,23 +127,12 @@ if ($blog_hero){
       <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
     </div><!-- /.carousel -->
     
-<<<<<<< HEAD
-    <!-- CIERRA CARRUSEL EVENTOS DESTACADOS -->       
-=======
     <!-- CIERRA CARRUSEL EVENTOS DESTACADOS -->          
-        </div>
-        <!-- Galerías -->    
-        <div class="clearfix row-fluid">
-          <div class=" span8 galeriaHoy">
->>>>>>> 3fac2707aef41b6d18b71cad7bc9fc8dda4aa331
 
           </div><!-- cierra .galeriaHoy -->   
 
           <div class="span4 galeriaManana">
-            <div class="span4 titManana hidden-phone">
-              <span>No te pierdas mañana...</span>  
-            </div><!-- cierra .titManana --> 
-          </div><!-- cierra .galeriaHoy -->   
+                      </div><!-- cierra .galeriaHoy -->   
         </div>
       </div><!-- cierra .hoy -->
 
